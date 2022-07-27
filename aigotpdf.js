b2f6505493460cfea3934eebd2f75f8b319259bd
Leoztalk <107846720+leoztalk@users.noreply.github.com>
2022-07-27 20:54:49 +0800

    Update aigotpdf.js

--- aigotpdf.js
+++ aigotpdf.js
@@ -45,7 +45,7 @@ var aigotpdf = function aigotpdf() {
         return document.getElementById(element);
     }
     var aigotpdf_mime = "application/x-aigotpdf";
-    var aigotpdf_chrome = "TokenSignings";
+    var aigotpdf_chrome = "TokenSigning";
     var USER_CANCEL = "user_cancel";
     var NO_CERTIFICATES = "no_certificates";
     var INVALID_ARGUMENT = "invalid_argument";
